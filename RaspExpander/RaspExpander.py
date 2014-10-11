e786ad04d4394f7b785a0d29dcaca266d77f1c8f
Hakan Lindestaf <hakan@lindestaf.com>
2014-10-10 20:51:27 -0700

    Fix logging

--- RaspExpander/RaspExpander.py
+++ RaspExpander/RaspExpander.py
@@ -31,7 +31,7 @@ formatter = logging.Formatter('%(asctime)s %(name)-12s: %(levelname)-8s %(messag
 # tell the handler to use this format
 console.setFormatter(formatter)
 # add the handler to the root logger
-logging.addHandler(console)
+logging.getLogger('').addHandler(console)
 
 logging.info('Starting')
 
