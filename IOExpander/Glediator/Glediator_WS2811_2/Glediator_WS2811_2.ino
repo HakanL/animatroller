09cd5749cc3aa2b29f92b5a88afa27549ba7e522
Hakan Lindestaf <hakan@lindestaf.com>
2014-11-30 16:43:39 -0800

    Fixed Glediator

--- IOExpander/Glediator/Glediator_WS2811_2/Glediator_WS2811_2.ino
+++ IOExpander/Glediator/Glediator_WS2811_2/Glediator_WS2811_2.ino
@@ -1,12 +1,12 @@
 #include "FastLED.h"
 
-#define NUM_LEDS 60
+#define NUM_LEDS 200
 #define DATA_PIN 5
 
 CRGB leds[NUM_LEDS];
 
 void setup() {
-  Serial.begin(230400);
+  Serial.begin(250000);
 
   FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
 }
@@ -20,9 +20,9 @@ void loop() {
   while (serialGlediator () != 1) {}
 
   for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i].r = serialGlediator ();
-    leds[i].g = serialGlediator ();
-    leds[i].b = serialGlediator ();
+    leds[i].r = serialGlediator();
+    leds[i].g = serialGlediator();
+    leds[i].b = serialGlediator();
   }
   
   FastLED.show();
